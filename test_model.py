8e6a3e2dc06f38a97076f788c94dac356b563420
Conor <conor.will.reid@gmail.com>
2018-10-07 09:05:38 -0400

    added get_lines test

--- test_model.py
+++ test_model.py
@@ -1,6 +1,13 @@
 import model
 import pytest
 import pandas as pd
+import unittest.mock as mock
+
+
+def construct_request_content():
+    response = mock.MagicMock()
+    response.content = mock.MagicMock(return_value='hello')
+    return response
 
 
 @pytest.mark.parametrize("test_input, expect", [(0, 10), (6, 7), (13, 4),
@@ -18,3 +25,14 @@ def test_defense_opponent_fusion():
     data_for_df = {'stat_thrown': [1, 1], 'stat_created': [2, 2]}
     df_data = pd.DataFrame(data=data_for_df)
     assert model.defense_opponent_fusion(df_data, 'stat')['stat'][0] == 1.4500000000000002
+
+
+@mock.patch('requests.get', return_value=construct_request_content())
+@mock.patch('json.loads',
+            return_value={'events': [
+                {'teams': [
+                    {'name': 'hello'}, {'name': 'goodbye'}],
+                 'lines': {'3': {'spread': {'point_spread_home': 1}, 
+                                 'total': {'total_under': 1}}}}]})
+def test_get_lines(get, loads):
+    assert model.get_lines()['team_name'][0] == 'hello'
