dcc90286ebd79f4ace6376895ec1877552d36809
Conor <conor.will.reid@gmail.com>
2018-10-07 14:38:49 -0400

    finished up logging and made logging output file

--- dst_scoring_model.py
+++ dst_scoring_model.py
@@ -10,6 +10,7 @@ import dst_scoring_model.model as model
 
 
 def main():
+    logging.basicConfig(filename='dst_log.log', level=logging.DEBUG)
     spreads = get_pinnacle_data.get_lines()
     tr_items = {}
     defense_pure_df = pd.DataFrame()
@@ -17,6 +18,7 @@ def main():
         tr_items[key] = get_tr_data.get_tr_stats_full(
             value['url'], value['column_name'])
 
+    logging.info('merging items together for defense_pure_df')
     defense_pure_df = (pd.merge(spreads, tr_items['sacks_defense_list'],
                        how='left', on='team_name'))
 
@@ -25,22 +27,26 @@ def main():
                                    tr_items[item],
                                    how='left', on='team_name')
 
+    logging.info('merging items together for opponent_pure_df')
     opponent_pure_df = pd.merge(tr_items['sacks_offense_list'],
                                 tr_items['interceptions_thrown'],
                                 how='left', on='team_name')
     opponent_pure_df = pd.merge(opponent_pure_df, tr_items['fumbles_thrown'],
                                 how='left', on='team_name')
 
+    logging.info('merging items for full fused_df')
     fused_df = pd.merge(defense_pure_df, opponent_pure_df, how='left',
                         left_on='opponent', right_on='team_name')
 
     for key, value in maps.poisson_events.items():
+        logging.info('getting poisson event probablity for ' + key)
         fused_df[key] = fused_df[key].apply(
             lambda row: model.poisson_create(row, value))
     
     for item in maps.defense_opponent_list:
         fused_df = clean_data.defense_opponent_fusion(fused_df, item)
 
+    logging.info('applying points_allowed_score')
     fused_df['points_allowed_score'] = fused_df['points_allowed'].apply(
         lambda row: model.points_allowed_score(row))
     logging.info('creating final score output')
