63b97d84ac8f65b476177a1bd48ccf33c25ec02c
conorreid <conor.will.reid@gmail.com>
2019-09-11 11:03:39 -0400

    add precommit hooks

--- dst_scoring_model.py
+++ dst_scoring_model.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime, date
 
 import pandas as pd
 
@@ -11,65 +12,90 @@ import dst_scoring_model.get_qb_data as get_qb_data
 
 
 def main():
-    logging.basicConfig(filename='dst_log.log', level=logging.DEBUG)
+    logging.basicConfig(filename="dst_log.log", level=logging.DEBUG)
     spreads = get_pinnacle_data.get_lines()
     tr_items = {}
     defense_pure_df = pd.DataFrame()
     for key, value in maps.tr_stat_list.items():
         tr_items[key] = get_tr_data.get_tr_stats_full(
-            value['url'], value['column_name'])
+            value["url"], value["column_name"]
+        )
 
-    logging.info('merging items together for defense_pure_df')
-    defense_pure_df = (pd.merge(spreads, tr_items['sacks_defense_list'],
-                       how='left', on='team_name'))
+    logging.info("merging items together for defense_pure_df")
+    defense_pure_df = pd.merge(
+        spreads, tr_items["sacks_defense_list"], how="left", on="team_name"
+    )
 
     for item in maps.defense_pure_list:
-        defense_pure_df = pd.merge(defense_pure_df,
-                                   tr_items[item],
-                                   how='left', on='team_name')
+        defense_pure_df = pd.merge(
+            defense_pure_df, tr_items[item], how="left", on="team_name"
+        )
 
-    logging.info('merging items together for opponent_pure_df')
+    logging.info("merging items together for opponent_pure_df")
     qb_interceptions = get_qb_data.get_footballdb_data()
-    int_fusion = pd.merge(tr_items['interceptions_thrown'],
-                          qb_interceptions, how='left')
-    int_fusion['interceptions_thrown'] = int_fusion.apply(
-        lambda row: clean_data.interceptions_merge(row), axis=1)
-    opponent_pure_df = pd.merge(tr_items['sacks_offense_list'],
-                                int_fusion,
-                                how='left', on='team_name')
-    opponent_pure_df = pd.merge(opponent_pure_df, tr_items['fumbles_thrown'],
-                                how='left', on='team_name')
+    int_fusion = pd.merge(
+        tr_items["interceptions_thrown"], qb_interceptions, how="left"
+    )
+    int_fusion["interceptions_thrown"] = int_fusion.apply(
+        lambda row: clean_data.interceptions_merge(row), axis=1
+    )
+    opponent_pure_df = pd.merge(
+        tr_items["sacks_offense_list"], int_fusion, how="left", on="team_name"
+    )
+    opponent_pure_df = pd.merge(
+        opponent_pure_df, tr_items["fumbles_thrown"], how="left", on="team_name"
+    )
 
-    logging.info('merging items for full fused_df')
-    fused_df = pd.merge(defense_pure_df, opponent_pure_df, how='left',
-                        left_on='opponent', right_on='team_name')
+    logging.info("merging items for full fused_df")
+    fused_df = pd.merge(
+        defense_pure_df,
+        opponent_pure_df,
+        how="left",
+        left_on="opponent",
+        right_on="team_name",
+    )
 
     for key, value in maps.poisson_events.items():
-        logging.info('getting poisson event probablity for ' + key)
+        logging.info("getting poisson event probablity for " + key)
         fused_df[key] = fused_df[key].apply(
-            lambda row: model.poisson_create(row, value))
+            lambda row: model.poisson_create(row, value)
+        )
 
     for item in maps.defense_opponent_list:
         fused_df = clean_data.defense_opponent_fusion(fused_df, item)
 
-    logging.info('applying points_allowed_score')
-    fused_df['points_allowed_score'] = fused_df['points_allowed'].apply(
-        lambda row: model.points_allowed_score(row))
-    logging.info('creating final score output')
-    fused_df['final'] = fused_df['points_allowed_score'] + \
-        fused_df['interceptions'] * 2 + fused_df['sacks'] * 1 + \
-        fused_df['fumbles'] * 2 + fused_df['defensive_touchdowns'] * 6
-    logging.info('formatting df')
-    fused_df = fused_df.drop('team_name_y', axis=1)
-    fused_df['team_name'] = fused_df['team_name_x']
-    fused_df = fused_df[['team_name', 'final', 'opponent',
-                         'points_allowed_score', 'interceptions',
-                         'fumbles', 'sacks', 'defensive_touchdowns']]
-    fused_df.sort_values(by='final', ascending=False, inplace=True)
-    logging.info('creating csv')
-    fused_df.to_csv('df_final.csv',
-                    index=False)
+    logging.info("applying points_allowed_score")
+    fused_df["points_allowed_score"] = fused_df["points_allowed"].apply(
+        lambda row: model.points_allowed_score(row)
+    )
+    logging.info("creating final score output")
+    fused_df["final"] = (
+        fused_df["points_allowed_score"]
+        + fused_df["interceptions"] * 2
+        + fused_df["sacks"] * 1
+        + fused_df["fumbles"] * 2
+        + fused_df["defensive_touchdowns"] * 6
+    )
+    logging.info("formatting df")
+    fused_df = fused_df.drop("team_name_y", axis=1)
+    fused_df["team_name"] = fused_df["team_name_x"]
+    fused_df = fused_df[
+        [
+            "team_name",
+            "final",
+            "opponent",
+            "points_allowed_score",
+            "interceptions",
+            "fumbles",
+            "sacks",
+            "defensive_touchdowns",
+        ]
+    ]
+    fused_df.sort_values(by="final", ascending=False, inplace=True)
+    logging.info("creating csv")
+    current_date = "{:%Y_%m_%d}".format(datetime.now())
+    fused_df.to_csv(f"df_final_{current_date}.csv", index=False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
